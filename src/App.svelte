53464b54d906e1826bddf5b769daf5da4ed2203d
Peter Miller <peter-miller@gmx.de>
2021-02-10 15:51:06 +0100

    Added writable count in App.svelte (For testing)

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script>
+	import {writable} from "svelte/store";
+
+	const count = writable(0);
+
+	count.plus = () => {
+		count.update(n => n+1);
+	}
+	count.minus = () => {
+		count.update(n => n-1);
+	}
+
 	let name = "Peter";
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Welcome to my ToDo App.</p>
+	<p>Der Count ist: {$count}</p>
+	<button on:click={count.plus}>+1</button>
+	<button on:click={count.minus}>-1</button>
 </main>
 
 <style>
